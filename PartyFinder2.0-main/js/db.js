0b64fdd736b41758884f6db9bf0c06e7f89ada45
Santydas <dsdazam@academia.usbbog.edu.co>
2025-05-21 23:12:09 +0000

    CambioDeCredenciales

--- PartyFinder2.0-main/js/db.js
+++ PartyFinder2.0-main/js/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'password',
-  database: 'partyfinder'
+  host: 'sql.freedb.tech',
+  user: 'freedb_party',
+  password: '8!P5QC&Y!fnv23@',
+  database: 'freedb_partyfinder'
 });
 
 // Conectar
